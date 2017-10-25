2e57ebc251ce013094407e3285b2085ec3997f77
Johannes K Becker <jkbecker@bu.edu>
2017-10-25 10:35:24 -0400

    updating ex1

--- exercise01/hellonode.cc
+++ exercise01/hellonode.cc
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <omnetpp.h>
 using namespace omnetpp;
-class Txc1 : public cSimpleModule
+class HelloNode : public cSimpleModule
 {
   protected:
     // The following redefined virtual function holds the algorithm.
@@ -9,21 +9,21 @@ class Txc1 : public cSimpleModule
     virtual void handleMessage(cMessage *msg) override;
 };
 // The module class needs to be registered with OMNeT++
-Define_Module(Txc1);
-void Txc1::initialize()
+Define_Module(HelloNode);
+void HelloNode::initialize()
 {
     // Initialize is called at the beginning of the simulation.
     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
     // to send the first message. Let this be `tic'.
     // Am I Tic or Toc?
-    if (strcmp("tic", getName()) == 0) {
+    if (strcmp("nodeA", getName()) == 0) {
         // create and send first message on gate "out". "tictocMsg" is an
         // arbitrary string which will be the name of the message object.
-        cMessage *msg = new cMessage("tictocMsg");
+        cMessage *msg = new cMessage("HelloWorldMsg");
         send(msg, "out");
     }
 }
-void Txc1::handleMessage(cMessage *msg)
+void HelloNode::handleMessage(cMessage *msg)
 {
     // The handleMessage() method is called whenever a message arrives
     // at the module. Here, we just send it to the other module, through
